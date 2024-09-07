42e3be8d89a0e33015896dfd774f2c341e27eafc
jameswpierce@gmail.com <jameswpierce@gmail.com>
2024-09-07 17:49:16 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -18,4 +18,5 @@ class Form1(Form1Template):
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     anvil.js.window.open("https://www.bing.com/chat", "_blank")
+    self.get_components()
     
