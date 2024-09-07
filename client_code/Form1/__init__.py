e216b22f71d07d8f4a9837c8e25184e4b2722df5
jameswpierce@gmail.com <jameswpierce@gmail.com>
2024-09-07 16:41:21 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.js.window
+
 
 
 class Form1(Form1Template):
@@ -8,3 +10,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.js.window.open("https://www.bing.com/chat", "_blank")
+    
